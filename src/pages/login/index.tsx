94099100699566850dfbf9f5f6f390d9d6e85664
Brendonap <brendonapitcher@gmail.com>
2022-01-26 16:42:53 +0200

    eslint errors

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useHistory } from "react-router";
 import Card from "../../components/card";
 import { googleLogin } from "../../firestore";
