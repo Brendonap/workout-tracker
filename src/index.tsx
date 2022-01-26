2641f30d7725ea5117f7e9d9cd47f92577bdda14
Brendonap <brendonapitcher@gmail.com>
2022-01-26 17:32:29 +0200

    added register service worker + cs change

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { appReducer, AppStateProvider } from "./store";
+import { register } from "./serviceWorkerRegistration";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,3 +19,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+register();
